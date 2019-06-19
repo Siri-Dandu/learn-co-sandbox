64c74738f9bbb5c5c7350daa2730f4dc3b6b493f
Siri-Dandu <sdandu99@gmail.com>
2019-06-19 13:02:16 +0000

    Automatically backed up by Learn

---
+++ Interpolation.rb
@@ -0,0 +1,7 @@
+puts "What is your name?"
+name = gets.chomp
+puts "How old are you?"
+age = gets.chomp
+puts "What is your favourite subject in school?"
+subject = gets.chomp
+puts "#{name} is #{age} years old and likes the subject of #{subject}."
